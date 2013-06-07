b80faa776c6474c4babbba210894c7fb62705c0b
hik13 <hik13@icloud.com>
2013-06-07 11:00:12 +0300

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,8 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
+
+  has_many :microposts, dependent: :destroy
   
   has_secure_password
 
@@ -26,6 +28,10 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
 
   private
 
